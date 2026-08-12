db1e11e36e1347166ec33ae614eac89b310f23ed
priyanshusalvi360-tech <priyanshusalvi360@gmail.com>
2026-08-12 14:12:39 +0530

    feat: add server/routes/projectRoutes.js

---
+++ server/routes/projectRoutes.js
@@ -0,0 +1,22 @@
+// routes/projectRoutes.js
+const express = require('express');
+const router = express.Router();
+const {
+  getProjects,
+  getProjectById,
+  createProject,
+  updateProject,
+  deleteProject,
+} = require('../controllers/projectController');
+const { protect } = require('../middleware/authMiddleware');
+
+// Public routes
+router.get('/', getProjects);
+router.get('/:id', getProjectById);
+
+// Protected admin routes
+router.post('/', protect, createProject);
+router.put('/:id', protect, updateProject);
+router.delete('/:id', protect, deleteProject);
+
+module.exports = router;
